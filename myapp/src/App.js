1ffcb9c26dce3563c48545e94209d8403224ec12
Hridoy <iamnirob334@gmail.com>
2022-10-09 00:07:01 +0600

    use callback and use memo source code uploaded

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,17 +1,26 @@
 import React, { useState } from "react";
-import MyComponent from "./components/MyComponent";
+import Button from "./components/Button";
+import ShowCount from "./components/ShowCount";
+import Title from "./components/Title";
 
 export default function App() {
-  const [show, setShow] = useState(true);
+  const [count1, setCount1] = useState(0);
+  const [count2, setCount2] = useState(0);
+  const increamentByOne = () => {
+    setCount1((prevCount) => prevCount + 1);
+  };
+  const increamentByFive = () => {
+    setCount2((prevCount) => prevCount + 5);
+  };
   return (
     <div>
-      <h1>I miss You mySelf</h1>
-      <div>{show && <MyComponent />}</div>
-      <p>
-        <button type="button" onClick={() => setShow((prevShow) => !prevShow)}>
-          {show ? "Hide post" : "Show post"}{" "}
-        </button>
-      </p>
+      <Title />
+      <ShowCount count={count1} title="Counter 1" />
+      <Button handleClick={increamentByOne}>Increament by one</Button>
+      <hr />
+      <ShowCount count={count2} title="Counter 2" />
+
+      <Button handleClick={increamentByFive}>Increament by Five</Button>
     </div>
   );
 }
---
+++ myapp/src/components/Button.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Button({ handleClick, children }) {
+  console.log(`rendering button ${children}`);
+  return (
+    <div>
+      <p>
+        <button type="button" onClick={handleClick}>
+          {children}
+        </button>
+      </p>
+    </div>
+  );
+}
+export default React.memo(Button);
