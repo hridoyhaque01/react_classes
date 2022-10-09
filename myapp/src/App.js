6fa44524f0553ec0b7efac511c2f39c53f8c12b1
Hridoy <iamnirob334@gmail.com>
2022-10-09 09:42:33 +0600

    useCallback and useMemo source code uploaded

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import React, { useCallback, useMemo, useState } from "react";
+
 import Button from "./components/Button";
 import ShowCount from "./components/ShowCount";
 import Title from "./components/Title";
@@ -6,16 +7,27 @@ import Title from "./components/Title";
 export default function App() {
   const [count1, setCount1] = useState(0);
   const [count2, setCount2] = useState(0);
-  const increamentByOne = () => {
+
+  const increamentByOne = useCallback(() => {
     setCount1((prevCount) => prevCount + 1);
-  };
-  const increamentByFive = () => {
+  }, []);
+
+  const increamentByFive = useCallback(() => {
     setCount2((prevCount) => prevCount + 5);
-  };
+  }, []);
+
+  // const isEvenOrOdd = () => count1 % 2 === 0;
+  const isEvenOrOdd = useMemo(() => {
+    let i = 0;
+    while (i < 10000000000) i += 1;
+    return count1 % 2 === 0;
+  }, [count1]);
+
   return (
     <div>
       <Title />
       <ShowCount count={count1} title="Counter 1" />
+      <span>{isEvenOrOdd ? "Even" : "Odd"}</span>
       <Button handleClick={increamentByOne}>Increament by one</Button>
       <hr />
       <ShowCount count={count2} title="Counter 2" />
