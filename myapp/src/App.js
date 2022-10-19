97b030a306ab2577bd974c475b5bfe93b0067c23
Hridoy <iamnirob334@gmail.com>
2022-10-19 21:08:13 +0600

    some changes

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,17 +1,18 @@
-import React, { useState } from "react";
+import React from "react";
 import MyComponent from "./components/MyComponent";
 
 export default function App() {
-  const [show, setShow] = useState(true);
+  // const [show, setShow] = useState(true);
   return (
     <div>
-      <h1>I miss You mySelf</h1>
+      <MyComponent />
+      {/* <h1>I miss You mySelf</h1>
       <div>{show && <MyComponent />}</div>
       <p>
         <button type="button" onClick={() => setShow((prevShow) => !prevShow)}>
           {show ? "Hide post" : "Show post"}{" "}
         </button>
-      </p>
+      </p> */}
     </div>
   );
 }
--- myapp/src/components/MyComponent.js
+++ myapp/src/components/MyComponent.js
@@ -2,13 +2,13 @@ import React, { useEffect, useState } from "react";
 
 export default function MyComponent() {
   const [count, setCount] = useState(0);
-  //   const [text, setText] = useState(".");
-  const [date, setDate] = useState(new Date());
+  const [text, setText] = useState(".");
+  // const [date, setDate] = useState(new Date());
 
-  const tick = () => {
-    console.log("clock ticking");
-    setDate(new Date());
-  };
+  // const tick = () => {
+  //   console.log("clock ticking");
+  //   setDate(new Date());
+  // };
 
   const addClick = () => {
     setCount((prevCount) => prevCount + 1);
@@ -19,27 +19,27 @@ export default function MyComponent() {
     document.title = `clicked ${count} times`;
   }, [count]);
 
-  useEffect(() => {
-    console.log("timer updating");
-    const interval = setInterval(tick, 1000);
+  // useEffect(() => {
+  //   console.log("timer updating");
+  //   const interval = setInterval(tick, 1000);
 
-    // clean up for component will unmount
-    return () => {
-      console.log("component unmounted");
-      clearInterval(interval);
-    };
-  }, []);
+  //   // clean up for component will unmount
+  //   return () => {
+  //     console.log("component unmounted");
+  //     clearInterval(interval);
+  //   };
+  // }, []);
 
   return (
     <div>
-      <p>Time : {date.toLocaleTimeString()}</p>
-      {/* <p>
+      {/* <p>Time : {date.toLocaleTimeString()}</p> */}
+      <p>
         <input
           type="text"
           value={text}
           onChange={(e) => setText(e.target.value)}
         />
-      </p> */}
+      </p>
       <p>
         <button onClick={addClick} type="button">
           Click
